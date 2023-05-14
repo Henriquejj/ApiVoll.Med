1c607598b00cab924d27410c98d9c564ae44cb61
henrique <henrique.jose@outlook.com>
2023-05-15 00:13:24 +0100

    criação  da classe MedicoController,

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/medicos")
-public class MedicoController {
+public class MedicoController{
 @PostMapping
 public void cadastrar(@RequestBody String json) {
     System.out.println(json);
